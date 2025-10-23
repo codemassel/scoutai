da205400349d3855b1c4618b4f140b4444a74365
codemassel <19mzimmermann92@gmail.com>
2025-10-24 01:47:55 +0200

    Added backend with spring initializr

---
+++ backend/src/main/java/com/scoutai/backend/ScoutaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scoutai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutaiBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/scoutai/backend/ScoutaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoutai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
